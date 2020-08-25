92e31c5b8a48b0d5a03f5b126d43c49f80164100
Shorya Singh Thakur <shoryathakur007@gmail.com>
2020-08-25 13:15:15 +0530

    first commit

---
+++ .history/src/components/Navbar_20200825131157.js
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+const Navbar = () =>{
+    return (
+        <nav className="nav-wrapper red darken-3">
+            <div className="container">
+                <a className="brand-logo">Poke'Times</a>
+            </div>
+        </nav>
+    )
+}
---
+++ .history/src/components/Navbar_20200825131438.js
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+const Navbar = () =>{
+    return (
+        <nav className="nav-wrapper red darken-3">
+            <div className="container">
+                <a className="brand-logo">Poke'Times</a>
+                <ul>
+                    <li><a href=""></a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
